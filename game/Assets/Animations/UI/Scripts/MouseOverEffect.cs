b6a0f4d5081eafc73d328b34e22adeef950955b1
Johan <johan.kiintola@gmail.com>
2018-08-05 01:35:23 +0300

    UI Updates

---
+++ game/Assets/Animations/UI/Scripts/MouseOverEffect.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOverEffect : MonoBehaviour {
+
+    public void PointerEnter() { transform.localScale = new Vector2(1.2f, 1.2f); }
+    public void PointerExit() { transform.localScale = new Vector2(1f, 1f); }
+}
